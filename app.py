d523342389cf7c7b1c00eff720b4d49a9d6301e2
yao-ying228 <wowowo228@msn.com>
2021-06-30 18:06:26 +0800

    update msg

--- app.py
+++ app.py
@@ -42,11 +42,11 @@ def handle_message(event):
 
     if msg = ['hi' , '你好' , '幹']
         r = '嗨'
-    elif msg = '機掰'
+    elif msg = '機掰' :
         r = '臭機掰'
-    elif msg = '鬧狗'
+    elif msg = '鬧狗' :
         r = '鬧小'
-    elif ms = '['幹' , 'pp' , '北藍']
+    elif ms = [  'pp' , '北藍']
         r = '真的很北藍'
 
     line_bot_api.reply_message(
