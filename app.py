7b36f25ef7bf952de59b37fa3413bd7df7bdeb24
yao-ying228 <wowowo228@msn.com>
2021-06-30 17:51:18 +0800

    update

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = 今天PP了嗎
+    s = '今天PP了嗎'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=s))
